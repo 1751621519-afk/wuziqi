4e98991205f9cdb23ee884cc93a517c6fec8260c
1751621519-afk <1751621519@qq.com>
2026-06-08 05:14:10 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
   build: {
-    sourcemap: 'hidden',
+    sourcemap: false,
+    cssCodeSplit: false,
   },
   plugins: [
-    react({
-      babel: {
-        plugins: [
-          'react-dev-locator',
-        ],
-      },
-    }),
+    react(),
     traeBadgePlugin({
       variant: 'dark',
       position: 'bottom-right',
@@ -25,7 +21,8 @@ export default defineConfig({
       clickUrl: 'https://www.trae.ai/solo?showJoin=1',
       autoTheme: true,
       autoThemeTarget: '#root'
-    }), 
-    tsconfigPaths()
+    }),
+    tsconfigPaths(),
+    viteSingleFile(),
   ],
 })
